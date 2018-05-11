31719fd1a05696b415eb0306087ac94263b490e5
chio-ken <chioken@outlook.com>
2018-05-11 22:41:03 +0900

    add swiper-icons

--- src/pages/home/components/Swiper.vue
+++ src/pages/home/components/Swiper.vue
@@ -15,27 +15,27 @@ export default {
   data () {
     return {
       swiperOption: {
-          pagination: '.swiper-pagination',
-          loop: true
+        pagination: '.swiper-pagination',
+        loop: true
       },
       swiperList: [{
-          id: '0001',
-          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/3c/eefaaf2d84acad02.jpg_750x200_bffe03ce.jpg'
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1805/3c/eefaaf2d84acad02.jpg_750x200_bffe03ce.jpg'
       }, {
-          id: '0002',
-          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/15/9250dbc86a456302.jpg_750x200_b423f532.jpg'
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/15/9250dbc86a456302.jpg_750x200_b423f532.jpg'
       }, {
-          id: '0003',
-          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1801/bd/04554e7c67650302.jpg_750x200_4293d60a.jpg'
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1801/bd/04554e7c67650302.jpg_750x200_4293d60a.jpg'
       }, {
-          id: '0004',
-          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/e5/82b53c47166cfc02.jpg_750x200_0d3a0066.jpg'
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1804/e5/82b53c47166cfc02.jpg_750x200_0d3a0066.jpg'
       }, {
-          id: '0005',
-          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b1/54862f4b1501e202.jpg_750x200_097bb6b3.jpg'
+        id: '0005',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/b1/54862f4b1501e202.jpg_750x200_097bb6b3.jpg'
       }, {
-          id: '0006',
-          imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/5c/969bfa7af00a2702.jpg_750x200_b99b8e07.jpg'
+        id: '0006',
+        imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/5c/969bfa7af00a2702.jpg_750x200_b99b8e07.jpg'
       }]
     }
   }
@@ -44,14 +44,13 @@ export default {
 
 <style lang="stylus" scoped>
   .wrapper >>> .swiper-pagination-bullet-active
-    background: #fff  !important
+    background: #fff
   .wrapper
+    overflow: hidden
     width: 100%
     height: 0
-    overflow: hidden
     padding-bottom: 31.25%
-    .swiper-container
-      background: #eee  
+    background: #eee
     .swiper-image
-      width: 100%
+      width: 120%
 </style>

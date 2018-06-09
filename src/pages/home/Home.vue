5a66f2c1fa02145da9382b97115e193d55acbf13
chio-ken <chioken@outlook.com>
2018-06-09 20:41:36 +0900

    add Weekend component

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -3,7 +3,8 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <home-icons></home-icons>
-    <hoem-recommend></hoem-recommend>
+    <home-recommend></home-recommend>
+    <home-weekend></home-weekend>
 </div>
 </template>
 
@@ -11,14 +12,16 @@
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
-import HoemRecommend from './components/Recommend'
+import HomeRecommend from './components/Recommend'
+import HomeWeekend from './components/Weekend'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
     HomeIcons,
-    HoemRecommend
+    HomeRecommend,
+    HomeWeekend
   }
 }
 </script>
